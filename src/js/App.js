1746500c24c3bc30ae1b69b52e1270d5523adf02
Aleksandr Mamontov <mamont861@yandex.ru>
2025-06-16 20:14:20 +0300

    third commit

--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,5 @@
-import Button from '../components/button/Button';
-import Popover from '../components/popover/Popover';
+import Button from '../components/button/button';
+import Popover from '../components/popover/popover';
 
 export default class App {
     constructor() {
