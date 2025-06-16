3a56e454bdd81bd09af8065817b5559771be699f
Aleksandr Mamontov <mamont861@yandex.ru>
2025-06-16 20:15:52 +0300

    correct lint

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -48,7 +48,7 @@ module.exports = {
             patterns: [
                 {
                     from: 'LICENSE', to: 'LICENSE'
-                } 
+                }
             ],
         }),
     ],
